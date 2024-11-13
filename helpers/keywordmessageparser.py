3d7e463f4c7e4c98a15130f72677d0cfa0695839
Ebag333 <ebagola@gmail,.com>
2024-11-13 00:20:15 -0800

    Linting!

--- helpers/keywordmessageparser.py
+++ helpers/keywordmessageparser.py
@@ -1,3 +1,7 @@
+"""
+This module provides help with parsing strings
+"""
+
 import re
 
 import helpers.helpers
@@ -6,12 +10,22 @@ BibleData = helpers.helpers.BibleBooks
 
 
 class KeywordMessageParser:
+
     def __init__(self, *, sefaria_index):
+        """
+
+        :param sefaria_index:
+        """
         self.christian_titles = BibleData.bible_books_list
         self.jewish_titles = [d["title"] for d in sefaria_index if "title" in d]
         self.jewish_titles = [title for title in self.jewish_titles if title not in self.christian_titles]
 
-    def parse_message(self, message: str):
+    def parse_message(self, message: str) -> list[tuple[int, str, str]]:
+        """
+
+        :param message:
+        :return:
+        """
         # Regular expression to match keyword followed by book/chapter reference
         pattern = r"\b({keywords})\s+(\d+(?:\.\d+|:\d+)*)"
 
